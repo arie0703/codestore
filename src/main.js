2ceee987bc9990405f815b4222b616f0ce9ae290
arie0703 <[ari0703sophia@gmail.com]>
2021-03-21 20:18:41 +0900

    Firebase authの実装

--- src/main.js
+++ src/main.js
@@ -1,5 +1,38 @@
+import firebase from "firebase";
+
+const config = {
+
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_MESUREMENT_ID
+}
+
+//firebase初期化処理
+firebase.initializeApp(config);
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+//ログインしているかどうかの判定
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      var uid = user.email;
+      console.log(uid);
+      console.log(firebase.auth().currentUser);
+      // ...
+    } else {
+      // User is signed out
+      // ...
+      console.log("no user")
+      console.log(firebase.auth().currentUser);
+    }
+});
---
+++ src/views/Login.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="login">
+    <div class="signup">
+
+      <p>ログイン / 新規登録</p>
+      <input type="email" class="form-control" placeholder="E-mail" v-model="mailaddress"/>
+      <input type="password" class="form-control" placeholder="Password" v-model="password"/>
+
+      <button @click="signIn" class="btn btn-primary">Sign In</button>
+      <button @click="signUp" class="btn btn-primary register">Register</button>
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+input {
+  display: block;
+  margin: 10px auto;
+  width: 300px;
+}
+.login {
+  text-align: center;
+  margin: 0 auto;
+  background: #efefef;
+  width: 70%;
+  padding: 30px;
+  border-radius: 10px;
+}
+button {
+  display: block;
+  margin: 10px auto;
+}
+
+.register {
+  background-color: orange;
+  border: orange;
+}
+</style>
+
+<script>
+
+import firebase from "firebase";
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      mailaddress: '',
+      password: ''
+    }
+  },
+  methods: {
+    signUp: function () {
+      firebase.auth().createUserWithEmailAndPassword(this.mailaddress, this.password)
+      .then(
+        alert('Success'),
+        this.$router.push('/')
+      )
+    },
+    signIn: function () {
+      firebase.auth().signInWithEmailAndPassword(this.mailaddress, this.password).then(
+        user => {
+          alert('Success!' + user)
+          this.$router.push('/')
+        },
+        err => {
+          alert(err)
+        }
+      )
+    }
+  }
+}
+</script>
+
+
